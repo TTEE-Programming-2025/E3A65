4ab59080f9f2e50d3e89e630194bf71a65017a54
Lance Schaik <lanceschaik@gmail.com>
2025-05-12 19:36:40 +0800

    Third attempts, changing the password system to using functions to make the main body look cleaner.

--- hw3.c
+++ hw3.c
@@ -6,30 +6,29 @@
 
 #define SIZE 9
 #define MAX_RESERVED 10 
+#define PASSWORD 2025
 
 char seats[SIZE][SIZE];
 char tempSeats[SIZE][SIZE];
 
-void cleanScreen(){ // Clean screen
-    system("cls"); 
-}
-
+// Initialise seats and tag 10 random seats as '*'
 void iniseats(){
     for(int i=0; i<SIZE; i++)
-       for(int j=0; SIZE; j++)
+       for(int j=0; j<SIZE; j++)
           seats[i][j]='-';
 
     int count=0;
     while(count<MAX_RESERVED){
-        int row=rand()%SIZE;
-        int col=rand()%SIZE;
-        if(seats[row][col]!='*'){
-            seats[row][col]='*';
+        int r=rand()%SIZE;
+        int c=rand()%SIZE;
+        if(seats[r][c]!='*'){
+            seats[r][c]='*';
             count++;
         }
     }
 }
 
+// Showing the seats 
 void showseats(){
     printf("\\123456789\n");
     for(int i=SIZE-1; i>=0; i--){
@@ -41,9 +40,37 @@ void showseats(){
     }
     printf("\nPress any key to return to main menu");
     getch();
-    cleanScreen();
+    system("cls");
 }
 
+// Showing the temporary seats with '@'
+void showTempSeats(){
+    printf("\\123456789\n");
+    for(int i=SIZE-1; i>=0; i--){
+        printf("%d", i+1);
+        for (int j=0; j<SIZE; j++){
+            printf("%c", tempSeats[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int passwordCheck(){
+    int input, attempt = 0;
+    while (attempt < 3){
+        printf("Enter the password: ");
+        scanf("%d", &input);
+        if (input == PASSWORD){
+            printf("Wwelcome!\n");
+            return(1);
+        } else{
+            printf("Wrong  password\n");
+            attempt++;
+        }
+    }
+    printf("Error, shutting down");
+    return 0;
+}
 
 int confirmContinue() { // Comfirming exiting the program
     char choice;
@@ -51,7 +78,7 @@ int confirmContinue() { // Comfirming exiting the program
         printf("Enter again (y/n)? ");
         scanf(" %c", &choice);
         if (choice == 'y' || choice == 'Y') {
-            cleanScreen();
+            system("cls");
             return 1;
         } else if (choice == 'n' || choice == 'N') {
             printf("Ending...\n");
@@ -64,9 +91,7 @@ int confirmContinue() { // Comfirming exiting the program
 
 int main() // Main body
 {
-    int password, i=0;
-    char input;
-
+    
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
@@ -88,49 +113,39 @@ int main() // Main body
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
     printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
 
-    while (i<3){ // Checking if it's still under 3 attpemts
-        printf("Enter the 4-digit password:");
-        scanf("%d", &password);
-        if (password==2025){ 
-            printf("Welcome!\n");
-            getchar();
-            break;
-                             
-            } else {
-            i++; // Minus one attempt
-            printf("Wrong password, reamining attempts:%d\n", 3 - i);
-        }
-    }
+    if (!passwordCheck()) return 0;
 
-    while (1) // Main body
-        {
-            system("cls"); // Cleaning screen
-            printf("------------------------------\n");
-            printf("| a. : Available seats       |\n");
-            printf("| b. : Arrange for you       |\n");
-            printf("| c. : Choose by yourself    |\n");
-            printf("| d. : Exit                  |\n");
-            printf("------------------------------\n");
-            getchar(); // waiting for user to type anything
-            scanf("%c", &input);
+    iniSeats();
 
-            switch(input){ // Deciding with funtion we are using
-                case 'a':
-                    showseats();
-                    iniseats();
-                    break;
-                case 'b':
-                    break;
-                case 'c':
-                    break;
-                case 'd':
-                    if (!confirmContinue()){
-                    return 0; 
-                    }   
-                default:
-                    printf("Wrong option enter again!\n");
-                    break;
-            }
+    char input;
+    do {
+        system("cls"); // Cleaning screen
+        printf("------------------------------\n");
+        printf("| a. : Available seats       |\n");
+        printf("| b. : Arrange for you       |\n");
+        printf("| c. : Choose by yourself    |\n");
+        printf("| d. : Exit                  |\n");
+        printf("------------------------------\n");
+        scanf("%c", &input);
+
+        system("cls");
+        switch(input){ // Deciding with funtion we are using
+            case 'a':
+                showseats();
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+            case 'd':
+                if (!confirmContinue()){
+                return 0; 
+                }   
+            default:
+                printf("Wrong option enter again!\n");
+                break;
         }
-        return  0;
+    } while(1);
+
+    return  0;
 }
