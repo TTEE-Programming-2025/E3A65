0964665757192152db29ff6f1fbdd3e921d27bd5
Lance Schaik <lanceschaik@gmail.com>
2025-05-30 12:10:23 +0800

    Second attempt, added the confimContinue funtion.

--- hw4.c
+++ hw4.c
@@ -4,6 +4,24 @@
 
 #define MAX 10
 
+// Comfirming exiting the program
+int confirmContinue() { 
+    char choice;
+    while (1) {
+        printf("Enter again (y/n)? ");
+        scanf(" %c", &choice);
+        if (choice == 'y' || choice == 'Y') {
+            system("cls");
+            return 1;
+        } else if (choice == 'n' || choice == 'N') {
+            printf("Ending...\n");
+            return 0;
+        } else {
+            printf("Warning: Enter y or n\n");
+        }
+    }
+}
+
 int main()
 {
     if(!login()) return 0;
